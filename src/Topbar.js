0976aa93ccebfac333fa695621ffeef1a88ec4e3
Your Name <Your Name>
2022-07-16 17:26:30 +0900

    topbar 까지

---
+++ src/Topbar.js
@@ -0,0 +1,25 @@
+import './App.css';
+
+function Topbar () {
+
+
+    return (
+        <nav>
+        <div>
+          정서희
+        </div>
+        <div className='top-menu-wrap'>
+          <ol>
+            <li><a>About</a></li>
+            <li><a>Experience</a></li>
+            <li><a>Work</a></li>
+            <li><a>Contact</a></li>
+          </ol>
+          <button className='resume-btn'>resume</button>
+        </div>
+
+      </nav>
+    )
+}
+
+export default Topbar
