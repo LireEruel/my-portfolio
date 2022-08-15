43913cc12ccdbdb43f2107e382304c1a432876d7
LireEruel <jsh001505@gmail.com>
2022-08-15 20:13:15 +0900

    카드에 기여도, 기술스택, notion 링크 추가

--- src/Screen/Card.js
+++ src/Screen/Card.js
@@ -1,12 +1,17 @@
 import { useState } from "react";
 import ReactCardFlip from "react-card-flip";
 import "../assets/card.css";
-function Card({ imgUrl, title, content }) {
+function Card({ imgUrl, title, content, contribution, stack, notionUrl }) {
   const [isFlipped, setFlip] = useState(false);
 
   const flipChange = (e) => {
     e.preventDefault();
     setFlip(!isFlipped);
+  };
+
+  const openWindow = (e) => {
+    e.preventDefault();
+    window.open(notionUrl);
   };
   return (
     <ReactCardFlip
@@ -14,13 +19,25 @@ function Card({ imgUrl, title, content }) {
       flipDirection="horizontal"
       className="card"
     >
-      <div className="card" onMouseEnter={flipChange}>
+      <div className="card" onMouseEnter={flipChange} onClick={openWindow}>
         <img src={imgUrl}></img>
       </div>
-      <div className="card" onMouseLeave={flipChange}>
-        <div>
+      <div className="card" onMouseLeave={flipChange} onClick={openWindow}>
+        <div className="card-content">
           <h2>{title}</h2>
           <p>{content}</p>
+          <h5>나의 기여도</h5>
+          <ul className="contribution-ul">
+            {contribution.map((contri, index) => {
+              return <li key={index}>{contri}</li>;
+            })}
+          </ul>
+          <h5>기술 스택</h5>
+          <ul className="stack-ul">
+            {stack.map((tech, index) => {
+              return <li key={index}>{tech}</li>;
+            })}
+          </ul>
         </div>
       </div>
     </ReactCardFlip>
